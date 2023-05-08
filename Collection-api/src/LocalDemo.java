ab3e7be12083fd789e0b32a1a1e70a683141d061
Shalom Aby Abraham <abrahamshalom18@gmail.com>
2023-05-08 09:44:40 +0530

    First Commit

---
+++ Collection-api/src/LocalDemo.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LocalDemo {
+	public static void main(String[] args) {
+	
+//		ResourceBundle labels= ResourceBundle.getBundle("labels");
+		
+//		ResourceBundle labels= ResourceBundle.getBundle("labels", Locale.FRANCE);	
+	
+		Locale desi=new Locale("hi");
+		ResourceBundle labels= ResourceBundle.getBundle("labels", desi);
+		
+		System.out.println(labels.getString("greeting"));
+		System.out.println(labels.getString("message"));
+	}
+}
